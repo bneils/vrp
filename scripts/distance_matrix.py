b8e0da83d632d1497888a139b696fc3bf91b45e3
Ben Neilsen <bpneilsen@gmail.com>
2025-02-05 13:53:21 -0500

    Adding progressbar and updating dependencies

--- scripts/distance_matrix.py
+++ scripts/distance_matrix.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import requests
 import json
 import sys
+
+import requests
+import tqdm
 
 HOST_IP = 'http://127.0.0.1:5000'
 
@@ -26,24 +28,27 @@ if __name__ == '__main__':
     # i < j.
     lines = []
 
-    # Let the first coordinate be the depot.
-    depot = coords[0]
-    for i in range(len(coords)):
-        row = [0] * (i + 1)
-        for j in range(i + 1, len(coords)):
-            coord0 = coords[i]
-            coord1 = coords[j]
-            # This is hacky, alternatives=3 forces it to take all 3 routes it comes up
-            # with and use the shortest one. OSRM only wants to return the fastest route
-            # and getting it to return the shortest route requires more work, but this hasn't
-            # failed me yet.
-            request_str = f'{HOST_IP}/route/v1/driving/{coord0[1]},{coord0[0]};{coord1[1]},{coord1[0]}?steps=true&alternatives=3&exclude=toll'
-            resp = requests.get(request_str)
-            obj = json.loads(resp.content)
-            shortest_route = min(obj['routes'], key=lambda r: r['distance'])
-            miles = round(0.6214 / 1000 * shortest_route['distance'], 4)
-            row.append(miles)
-        lines.append(' '.join(map(str, row)))
+    num_calls = len(coords) * (len(coords) - 1) // 2
+    with tqdm.tqdm(total=num_calls) as pbar:
+        # Let the first coordinate be the depot.
+        depot = coords[0]
+        for i in range(len(coords)):
+            row = [0] * (i + 1)
+            for j in range(i + 1, len(coords)):
+                coord0 = coords[i]
+                coord1 = coords[j]
+                # This is hacky, alternatives=3 forces it to take all 3 routes it comes up
+                # with and use the shortest one. OSRM only wants to return the fastest route
+                # and getting it to return the shortest route requires more work, but this hasn't
+                # failed me yet.
+                request_str = f'{HOST_IP}/route/v1/driving/{coord0[1]},{coord0[0]};{coord1[1]},{coord1[0]}?steps=true&alternatives=3&exclude=toll'
+                resp = requests.get(request_str)
+                obj = json.loads(resp.content)
+                shortest_route = min(obj['routes'], key=lambda r: r['distance'])
+                miles = round(0.6214 / 1000 * shortest_route['distance'], 4)
+                row.append(miles)
+                pbar.update(1)
+            lines.append(' '.join(map(str, row)))
 
     content = '\n'.join(lines)
 
